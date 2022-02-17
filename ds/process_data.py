c1096deb852f8f6aad590e60e8208364708610de
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2022-02-17 18:28:33 +0300

    Add abbreviation replacer and lemmatizer

--- ds/process_data.py
+++ ds/process_data.py
@@ -13,6 +13,7 @@ from typing import NoReturn, Callable
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+import spacy
 
 
 def get_rating_from_filename(filename: str) -> int:
@@ -153,6 +154,95 @@ def clean_str(data: str) -> str:
         data = func(data)
 
     return data
+
+
+def replace_abbr(data: str) -> str:
+    """
+    Replace some abbreviations and shrink spaces to one.
+
+    >>> replace_abbr("It   doesn't    matter")
+    'it does not matter'
+    """
+
+    abbr_dict = {
+        "what's": "what is",
+        "what're": "what are",
+        "who's": "who is",
+        "who're": "who are",
+        "where's": "where is",
+        "where're": "where are",
+        "when's": "when is",
+        "when're": "when are",
+        "how's": "how is",
+        "how're": "how are",
+
+        "i'm": "i am",
+        "we're": "we are",
+        "you're": "you are",
+        "they're": "they are",
+        "it's": "it is",
+        "he's": "he is",
+        "she's": "she is",
+        "that's": "that is",
+        "there's": "there is",
+        "there're": "there are",
+
+        "i've": "i have",
+        "we've": "we have",
+        "you've": "you have",
+        "they've": "they have",
+        "who've": "who have",
+        "would've": "would have",
+        "not've": "not have",
+
+        "i'll": "i will",
+        "we'll": "we will",
+        "you'll": "you will",
+        "he'll": "he will",
+        "she'll": "she will",
+        "it'll": "it will",
+        "they'll": "they will",
+
+        "isn't": "is not",
+        "wasn't": "was not",
+        "aren't": "are not",
+        "weren't": "were not",
+        "can't": "can not",
+        "couldn't": "could not",
+        "don't": "do not",
+        "didn't": "did not",
+        "shouldn't": "should not",
+        "wouldn't": "would not",
+        "doesn't": "does not",
+        "haven't": "have not",
+        "hasn't": "has not",
+        "hadn't": "had not",
+        "won't": "will not",
+        r"\s+": " ",  # replace multi space with one single space
+    }
+
+    data = data.lower()
+
+    for pattern, repl in abbr_dict.items():
+        data = re.sub(pattern, repl, data)
+
+    return data
+
+
+def lemmatize(data: str) -> str:
+    """
+    Lemmatize input text.
+
+    >>> lemmatize("I was reading the paper.")
+    'I be read the paper .'
+    """
+
+    nlp = spacy.load("en_core_web_sm")
+    lemmatizer = nlp.get_pipe("lemmatizer")
+    doc = nlp(data)
+    res = " ".join(token.lemma_ for token in doc)
+
+    return res
 
 
 if __name__ == "__main__":
